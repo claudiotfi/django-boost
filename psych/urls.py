ca317f8593194888542d43c48d30f40a845e3ba3
Claudio <claudiotf@gmail.com>
2023-05-25 00:17:21 -0300

    tailwind

--- psych/urls.py
+++ psych/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path
+from django.shortcuts import redirect
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import dashboard, config
 
 urlpatterns = [
+    path('', lambda request: redirect('/dashboard/')),  # Redireciona '/' para '/dashboard/'
     path('admin/', admin.site.urls),
     path('dashboard/', dashboard, name='dashboard'),
     path('config/', config, name='config')
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
